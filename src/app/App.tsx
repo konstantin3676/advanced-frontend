48e4ed04a57246cccc7576a9cd869fe461fdd2ee
Konstantin <93384228+konstantin3676@users.noreply.github.com>
2023-10-12 09:51:49 +0300

    Lesson 10

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,11 @@
 import './styles/index.scss';
 
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 import { Suspense } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
-
-import { classNames } from './helpers/classNames/classNames';
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import { useTheme } from './theme/useTheme';
+import { classNames } from 'shared/lib/classNames/classNames';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -18,8 +17,8 @@ const App = () => {
       <Link to='/about'>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path='/' element={<MainPageAsync />} />
-          <Route path='/about' element={<AboutPageAsync />} />
+          <Route path='/' element={<MainPage />} />
+          <Route path='/about' element={<AboutPage />} />
         </Routes>
       </Suspense>
     </div>
