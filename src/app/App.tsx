2f9deae602e268d377a015a03a578b04055ad385
Konstantin <93384228+konstantin3676@users.noreply.github.com>
2023-10-12 09:52:25 +0300

    Lesson 11

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
 import './styles/index.scss';
 
+import { AppRouter } from 'app/providers/router';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
-import { Suspense } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { classNames } from 'shared/lib/classNames/classNames';
 
 const App = () => {
@@ -15,12 +13,7 @@ const App = () => {
       <button onClick={() => toggleTheme()}>TOGGLE</button>
       <Link to='/'>Главная</Link>
       <Link to='/about'>О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path='/' element={<MainPage />} />
-          <Route path='/about' element={<AboutPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
