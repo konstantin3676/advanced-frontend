3b1d5910a1b3dbcd8ebcf142d472a1b6fbcc7caa
Konstantin <93384228+konstantin3676@users.noreply.github.com>
2024-04-11 18:00:25 +0300

    Lesson 47

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,16 @@ import './styles/index.scss';
 
 import { Box, Flex } from '@chakra-ui/react';
 import { AppRouter } from 'app/providers/AppRouter';
-import { userActions } from 'entities/User';
+import { getUserInited, userActions } from 'entities/User';
 import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 
 export const App = () => {
   const dispatch = useAppDispatch();
+  const inited = useSelector(getUserInited);
 
   useEffect(() => {
     dispatch(userActions.initAuthData());
@@ -20,7 +22,7 @@ export const App = () => {
       <Navbar />
       <Flex>
         <Sidebar />
-        <AppRouter />
+        {inited && <AppRouter />}
       </Flex>
     </Box>
   );
--- src/app/providers/AppRouter/ui/AppRouter.tsx
+++ src/app/providers/AppRouter/ui/AppRouter.tsx
@@ -1,39 +1,35 @@
-import { Suspense, useMemo } from 'react';
+import { Suspense, useCallback } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import {
+  AppRoutesProps,
+  routeConfig,
+} from 'shared/config/routeConfig/routeConfig';
 import { PageLoader } from 'widgets/PageLoader';
 import { Box } from '@chakra-ui/react';
-import { useSelector } from 'react-redux';
-import { getUserAuthData } from 'entities/User';
+import { RequireAuth } from 'app/providers/AppRouter/ui/RequireAuth';
 
 export const AppRouter = () => {
-  const isAuth = useSelector(getUserAuthData);
+  const renderWithWrapper = useCallback((route: AppRoutesProps) => {
+    const element = (
+      <Box flexGrow={1} p={5}>
+        {route.element}
+      </Box>
+    );
 
-  const routes = useMemo(() => {
-    return routeConfig.filter(({ authOnly }) => {
-      if (authOnly) {
-        return isAuth;
-      }
-
-      return true;
-    });
-  }, [isAuth]);
+    return (
+      <Route
+        key={route.path}
+        path={route.path}
+        element={
+          route.authOnly ? <RequireAuth>{element}</RequireAuth> : element
+        }
+      />
+    );
+  }, []);
 
   return (
     <Suspense fallback={<PageLoader />}>
-      <Routes>
-        {routes.map(({ path, element }) => (
-          <Route
-            key={path}
-            path={path}
-            element={
-              <Box flexGrow={1} p={5}>
-                {element}
-              </Box>
-            }
-          />
-        ))}
-      </Routes>
+      <Routes>{Object.values(routeConfig).map(renderWithWrapper)}</Routes>
     </Suspense>
   );
 };
