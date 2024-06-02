0382e622afe129701cefac74e226e5b6f4ff3992
Konstantin <93384228+konstantin3676@users.noreply.github.com>
2024-06-02 15:20:37 +0300

    Lesson 80

--- src/features/NotificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/NotificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -1,4 +1,8 @@
 import {
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerOverlay,
   IconButton,
   Popover,
   PopoverBody,
@@ -6,26 +10,51 @@ import {
   PopoverTrigger,
 } from '@chakra-ui/react';
 import { NotificationList } from 'entities/Notification';
+import { useState } from 'react';
+import { BrowserView, MobileView } from 'react-device-detect';
 import { MdOutlineNotifications } from 'react-icons/md';
 
 export const NotificationButton = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const trigger = (
+    <IconButton
+      isRound
+      aria-label='Notifications'
+      variant='ghost'
+      colorScheme='teal'
+      size='sm'
+      icon={<MdOutlineNotifications size={24} />}
+      onClick={handleToggle}
+    />
+  );
   return (
-    <Popover isLazy>
-      <PopoverTrigger>
-        <IconButton
-          isRound
-          aria-label='Notifications'
-          variant='ghost'
-          colorScheme='teal'
-          size='sm'
-          icon={<MdOutlineNotifications size={24} />}
-        />
-      </PopoverTrigger>
-      <PopoverContent>
-        <PopoverBody maxH='320px' overflowY='auto'>
-          <NotificationList />
-        </PopoverBody>
-      </PopoverContent>
-    </Popover>
+    <>
+      <BrowserView>
+        <Popover isLazy>
+          <PopoverTrigger>{trigger}</PopoverTrigger>
+          <PopoverContent>
+            <PopoverBody maxH='320px' overflowY='auto'>
+              <NotificationList />
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
+      </BrowserView>
+      <MobileView>
+        {trigger}
+        <Drawer placement='bottom' onClose={handleToggle} isOpen={isOpen}>
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerBody maxH='320px' overflowY='auto'>
+              <NotificationList />
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+      </MobileView>
+    </>
   );
 };
